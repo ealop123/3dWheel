ef33a764ca5768ae9fc81fea33f44d1cd30a6e4a
Ezekiel Lopez <EzekielAdamLopez@Gmail.com>
2021-12-15 21:21:08 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", async function main() {
+  const $ = (str, dom = document) => [...dom.querySelectorAll(str)];
+  const wait = t => new Promise(r => setTimeout(r, t));
+  
+  const circle = document.querySelector('.circle');
+  const text = circle.innerHTML;// Note I am being lazy here and assuming the string has no unwanted whitespace
+  circle.innerHTML = '';
+  circle.style.setProperty('--numchs', text.length);
+  for ( let i = 0; i < text.length; i++ ) {
+    circle.innerHTML =
+      circle.innerHTML +
+      '<div class="char" style="--char-index: ' +
+      i +
+      ';">' +
+      text.charAt(i) +
+      '</div>';
+  }
+});
